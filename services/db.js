3e5d7c755acca4929271c8139f32e5ef3dd649e0
AbrahamChumaceroGaite <abraham.chumacero@ucb.edu.bo>
2023-10-20 09:22:38 -0300

    new-one

--- services/db.js
+++ services/db.js
@@ -1,10 +1,18 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
+/* const connection = mysql.createConnection({
   host: 'cljf4js2800mznqd6o8b43qyk',
   user: 'root',
   password: 'password@2022',
   database: 'monitor'
+}); */
+
+const connection = mysql.createConnection({
+  host: 'clnymjgu300s6pmcgihpxraxv',
+  user: 'clnymjgu10hh3cgpm7yie35j8',
+  password: 'xw8LqMzkm7ZCE0QFOOk80WqJ',
+  database: 'monitor'
 });
+
 
 module.exports = connection;
